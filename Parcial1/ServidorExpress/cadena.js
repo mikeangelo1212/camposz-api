bf76faf068452c135177e038d12a602e36ebcfbb
= <=>
2022-09-27 08:30:13 -0500

    practicas

---
+++ Parcial1/ServidorExpress/cadena.js
@@ -0,0 +1,9 @@
+
+    const pasarMayusculas = (cadena) => {return cadena.toUpperCase()}//otra manera de hacerlo
+    function quitarEspacios(cadena){return cadena.replace(/ -/g,"")}
+    function obtenerLongitud(cadena){return cadena.length}
+
+    //el exports hace publicas nuestras funciones, para sacarlos del archivo
+    exports.pasarMayusculas=pasarMayusculas;
+    exports.quitarEspacios=quitarEspacios;
+    exports.obtenerLongitud=obtenerLongitud;
--- Parcial1/ServidorExpress/express.js
+++ Parcial1/ServidorExpress/express.js
@@ -1,13 +1,70 @@
 const express = require('express');
-const cors =require('cors')
-
+const cors =require('cors');
+const { json } = require('express');
 const app = express();
-app.use(cors({origin:"http://localhost"}))
+const cadena = require('./cadena')
+const port=8081;
 
+app.use(cors({origin:"http://localhost"}))
+app.use(express.text())
+app.use(express.json())//middleware
+
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
+
+app.use((req, res,next)=> {
+    console.log("Esta es un middleware")
+    next()  
+},(req, res,next)=> {
+    console.log("Segunda funcion middleware")
+    next()
+})
 
 app.get('/', (req, res) => {
     //res.send('Servidor Express contestando: 8081')
     res.sendFile('./static/index.html',{root:__dirname},(err)=>{console.log('Archivo enviado')})
+})
+
+app.get('/suma', (req, res) => {
+    console.log(req.query)
+    let suma=parseInt(req.query.x)+parseInt(req.query.y)
+    res.send('La suma es '+ suma)
+})
+
+app.get('/mayusculas/:cadena', (req, res) => {
+    console.log(req.params)
+    res.send(req.params)
+})
+
+app.post('/texto', (req,res)=>{
+    console.log(req.body)
+    let may =cadena.pasarMayusculas(req.body)
+    let sinesp=cadena.quitarEspacios(req.body)
+    let longi =cadena.obtenerLongitud(req.body)
+    // let may = req.body.toUpperCase()
+    // let sinesp=req.body.trim()
+    // let longi =req.body.length
+    res.json({  mayusculas:may,
+                sinespacios: sinesp,
+                longitud:longi
+    })
+})
+
+app.post('/json', (req,res)=>{
+    console.log(req.body.nombre)
+    let cadena = 'Hola '+req.body.nombre+" "+req.body.apellido+" como estas?"
+    res.json({saludo:cadena})
+    
 })
 
 app.post('/', (req, res) => {
